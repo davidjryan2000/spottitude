cfde7b92655640d20d91e3fdbcc68e9d908ae3ef
David Ryan <davidjryan2000@gmail.com>
2017-09-18 10:40:37 +0100

    correct paths

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,14 @@
-import {ModuleWitProviders}from '@angular/core';
+import {ModuleWithProviders}from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {About Components} from './components/about/about.component';
+import {AboutComponent} from './components/about/about.component';
 import {SearchComponent} from './components/search/search.component';
 
 const appRoutes: Routes = [
     {
         path:'',
         component:SearchComponent
-    },
+    }
+    ,
     {
         path:'about',
         component:AboutComponent
